0df1d0b5ac16241ff73554e6554b6f4c56000ddc
Brandon Eichler <beedat1@gmail.com>
2014-12-13 21:10:55 -0600

    getNewSize moved to it's own function and slight UI improvements to the prompt

--- javascript/sketchpad.js
+++ javascript/sketchpad.js
@@ -1,6 +1,6 @@
 // default sketchpadWidth
 var sketchpadWidth = 16;
-
+var newSize;
 // builds the sketchpad
 function createSketchpad(width) {
     var squareSize = $("#sketchpad").width() / width;
@@ -27,10 +27,32 @@ function clearBoard() {
     $(".square").removeClass("square-normal");
 }
 
+// get the new size, input by the user by clicking the "Change Board Size" button
+function getNewSize(newSize) {
+    newSize = prompt("Please enter a new size for the sketch board (1 to 64).\nLeave blank for default size.");
+    // if the prompt is left blank then resize to default size
+    if (newSize === null) {
+        return;
+    }
+    else if (newSize === ""){
+        changeSize(sketchpadWidth);
+    }
+    // make sure the user entered a valid number
+    else if (isNaN(newSize) || newSize < 1 || newSize > 64) {
+        alert('"' + newSize + '" is not a valid NUMBER, try again.');
+    }
+    // otherwise, change the size of the sketchpad
+    else {
+        changeSize(newSize);
+    };
+    // TODO!
+    squareChange();
+}
+
 // change the board size
 function changeSize(width) {
     $("#sketchpad").empty();
-    createSketchpad(width);
+    createSketchpad(width);   
 }
 
 $(document).ready(function(){
@@ -45,20 +67,9 @@ $(document).ready(function(){
         clearBoard();
     });
     
+    // allows the user to resize the board by clicking the "Change Board Size" button
     $(".size-button").on("click", function() {
-        var newSize = prompt("Please enter a new size for the sketch board (1 to 64). Leave blank for default size.");
-        
-        if (isNaN(newSize) || newSize < 1 || newSize > 64) {
-            alert("That is not a valid NUMBER, try again.");
-            // return?
-        }
-        else if (newSize != null) {
-            changeSize(newSize);
-        } else {
-            changeSize(sketchpadWidth);
-        };
-        
-        squareChange();
+        getNewSize();
     });
     
     // necessary to make the bootstrap buttons clear the active state immediately after clicking them
