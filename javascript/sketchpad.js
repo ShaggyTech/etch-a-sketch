2af0f32799d287c052fe548f7630bf283abfe819
Brandon Eichler <beedat1@gmail.com>
2014-12-14 19:57:06 -0600

    incremental opacity won't turn off when in other modes

--- javascript/sketchpad.js
+++ javascript/sketchpad.js
@@ -1,6 +1,6 @@
 var sketchpadWidth = 16;  // default sketchpad width
 var newSize;
-var currentDrawMode = "defaultSquares";
+var currentDrawMode = 1;
 
 // builds the sketchpad
 function createSketchpad(width) {
@@ -24,66 +24,73 @@ function disableBoardColorChange() {
     });
 }
 
-function squareDefault() {
-    $("#sketchpad").mousedown(function(){
-        $(".square").hover(function(){
-            $(this).css("background-color", "blue");
-            $(".instructions-text").hide();
-        });
-    });
-    disableBoardColorChange();
-}
-
-function squareRandomColors() {
-    $("#sketchpad").mousedown(function(){
-        $(".square").hover(function(){
-            var color = randomColor(); 
-            $(this).css("background-color", color);
-            $(".instructions-text").hide();
-        });
-    });
-    disableBoardColorChange();
-}
-
-// generate a random color
-function randomColor(){
-    var hue = "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")";
-    return hue;
-}
-
 // clear the board, revert to default square color
 function clearBoard() {
-    $(".square").css("background-color", "#DEDEDE");
+    $("#sketchpad").empty();
+    createSketchpad(sketchpadWidth);
 }
 
 // selects the color change on the squares based on the currently selected mode
-function drawMode() {
-    if (currentDrawMode === "defaultSquares") {
-        $(".dropdown-menu > li").show();
-        $(".default").hide();
-        $("#mode-menu-text").text("Draw Mode: Default");
-        clearBoard();
-        squareDefault();
-    }
-    else if (currentDrawMode === "randomSquares") {
-        $(".dropdown-menu > li").show();
-        $(".random").hide();
-        $("#mode-menu-text").text("Draw Mode: Random Colors");
-        clearBoard();
-        squareRandomColors();
+function drawMode(mode) {
+    switch(mode) 
+    {
+        case 1: $(".dropdown-menu > li").show();
+                $(".default").hide();
+                $("#mode-menu-text").text("Draw Mode: Default");
+                clearBoard();
+                $("#sketchpad").on("mousedown", function(){
+                    $(".square").on("mouseover", function(){
+                        $(this).css("background-color", "blue");
+                        $(".instructions-text").hide();
+                    });
+                });
+                disableBoardColorChange();
+                break;
+
+        case 2: $(".dropdown-menu > li").show();
+                $(".random").hide();
+                $("#mode-menu-text").text("Draw Mode: Random Colors");
+                clearBoard();
+                $("#sketchpad").on("mousedown", function(){
+                    $(".square").on("mouseover", function(){
+                        var color = randomColor();
+                        $(this).css("background-color", color);
+                        $(".instructions-text").hide();
+                    });
+                });
+                disableBoardColorChange();
+                break;
+
+        case 3: $(".dropdown-menu > li").show();
+                $(".increment").hide();
+                $("#mode-menu-text").text("Draw Mode: Incremental Opacity");
+                clearBoard();
+                $("#sketchpad").on("mousedown", function(){
+                    $(".square").on("mouseover", function(){
+                        $(this).css({"background-color": "blue", "opacity": $(this).css("opacity") * 0.75});
+                        $(".instructions-text").hide();
+                    });
+                });
+                disableBoardColorChange();
+                break;
     };
 }
 
 // change the draw mode when a "Draw Mode" menu item is selected
 function changeDrawMode() {
     $(".default").on("click", function(){
-        currentDrawMode = "defaultSquares";
-        drawMode();
+        currentDrawMode = 1;
+        drawMode(currentDrawMode);
     });
     
     $(".random").on("click", function(){
-        currentDrawMode = "randomSquares";
-        drawMode();
+        currentDrawMode = 2;
+        drawMode(currentDrawMode);
+    });
+    
+    $(".increment").on("click", function(){
+        currentDrawMode = 3;
+        drawMode(currentDrawMode);
     });
 }
 //
@@ -91,22 +98,27 @@ function changeDrawMode() {
 // get the new size, input by the user by clicking the "Change Board Size" button
 function getNewSize(newSize) {
     newSize = prompt("Please enter a new size (1-64) for the sketch board.\nLeave blank for default size.");
-    // if the prompt is left blank then resize to default size
+    
+    // if the prompt is canceled then exit the function
     if (newSize === null) {
         return;
     }
+    // if the prompt is left blank then resize to default size
     else if (newSize === ""){
+        sketchpadWidth = 16;
         changeSize(sketchpadWidth);
     }
     // make sure the user entered a valid number
     else if (isNaN(newSize) || newSize < 1 || newSize > 64) {
         alert('"' + newSize + '" is not a valid NUMBER, try again.');
     }
-    // otherwise, change the size of the sketchpad
+    // otherwise, change the size of the sketchpad based on user input
     else {
-        changeSize(newSize);
+        sketchpadWidth = newSize;
+        changeSize(sketchpadWidth);
+        
     };
-    drawMode();
+    drawMode(currentDrawMode);
 }
 
 // change the board size
@@ -115,12 +127,18 @@ function changeSize(width) {
     createSketchpad(width);
 }
 
-function listeners() {
+// generate a random color
+function randomColor(){
+    var hue = "rgb(" + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + "," + (Math.floor(Math.random() * 256)) + ")";
+    return hue;
+}
+
+$(document).ready(function(){
     // draws the sketchpad and takes the sketchpadWidth as input
     createSketchpad(sketchpadWidth);
-    
+
     // changes the color of the squares on mouseover (default function)
-    drawMode();
+    drawMode(currentDrawMode);
     
     // listens for clicks on the draw mode drop-down menu
     changeDrawMode();
@@ -139,8 +157,4 @@ function listeners() {
     $(".btn").on("mouseup", function(){
         $(this).blur();
     });
-}
-
-$(document).ready(function(){
-    listeners();
 })
